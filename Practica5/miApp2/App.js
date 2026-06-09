58d34048aedd84ef05bbfd97cfb90cf0d56daa40
ailingumc-collab <ailingumc@gmail.com>
2026-06-09 02:17:26 -0600

    Componente propio practica6

--- Practica5/miApp2/App.js
+++ Practica5/miApp2/App.js
@@ -1,8 +1,11 @@
 /* Zona 1: importaciones de archivos y componentes  */
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Image} from 'react-native';
+
 import {Saludo} from './components/Saludo';
 import { Saludo2 } from './components/Saludo2';
+
+import { Perfil } from './components/Perfil';
 
 /* Zona 2: Main – Componentes */
 export default function App() {
@@ -17,8 +20,11 @@ export default function App() {
       <Text>--------------------------------</Text>
       <Saludo2/>
 
-      <StatusBar style="auto" />
+      <Text>--------------------------------</Text>
+      <Perfil/>
 
+      <StatusBar style="auto" />
+      
     </View>
   );
 }
---
+++ Practica5/miApp2/components/Perfil.js
@@ -0,0 +1,12 @@
+import {View, Text} from 'react-native';
+
+export const Perfil = () =>{
+    return(
+        <View>
+            <Text>Guzman Morales Cintia Ailin</Text>
+            <Text>Ingeniería en Sistemas Computacionales</Text>
+            <Text>Programación Móvil</Text>
+            <Text>Cuatrimestre: 9°</Text>
+        </View>
+    )
+}
