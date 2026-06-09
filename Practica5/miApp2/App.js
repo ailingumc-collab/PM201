b17b2a5a4de7722acd79238a25524b37f4b3f496
ailingumc-collab <ailingumc@gmail.com>
2026-06-09 15:40:25 -0600

    Practica7

--- Practica5/miApp2/App.js
+++ Practica5/miApp2/App.js
@@ -4,24 +4,15 @@ import { StyleSheet, Text, View, Image} from 'react-native';
 
 import {Saludo} from './components/Saludo';
 import { Saludo2 } from './components/Saludo2';
-
 import { Perfil } from './components/Perfil';
 
 /* Zona 2: Main – Componentes */
 export default function App() {
   return (
     <View style={styles.container}>
-      
-      <Image source={require('./assets/wave.png')} />
-      <Text>Hola Mundo React Native </Text>
-      <Text>--------------------------------</Text>
-      <Saludo/>
 
-      <Text>--------------------------------</Text>
-      <Saludo2/>
-
-      <Text>--------------------------------</Text>
-      <Perfil/>
+      <Perfil nombre="Cintia" carrera="ISC" materia="Móvil" cuatri="9"/>
+       <Perfil nombre="Ailin" carrera="ISC" materia="Móvil" cuatri="9"/>
 
       <StatusBar style="auto" /> 
       
--- Practica5/miApp2/components/Perfil.js
+++ Practica5/miApp2/components/Perfil.js
@@ -1,12 +1,38 @@
-import {View, Text} from 'react-native';
 
-export const Perfil = () =>{
+/* PERFIL usando Props  */
+/* import {View, Text} from 'react-native';
+
+export const Perfil = (props) =>{
     return(
         <View>
-            <Text>Guzman Morales Cintia Ailin</Text>
-            <Text>Ingeniería en Sistemas Computacionales</Text>
-            <Text>Programación Móvil</Text>
-            <Text>Cuatrimestre: 9°</Text>
+            <Text>{props.nombre} </Text>
+            <Text>{props.carrera}</Text>
+            <Text>{props.materia}</Text>
+            <Text>{props.cuatri}</Text>
+        </View>
+    )
+} */
+
+
+/* PERFIL usando Destructuración */
+import {View, Text, Button} from 'react-native';
+import React,{useState} from 'react';
+
+export const Perfil = ( {nombre, carrera, materia, cuatri}  ) =>{
+    const [mostrar, setMostrar]= useState(false)
+
+    return(
+        <View>
+            <Text>{nombre} </Text>
+            {mostrar &&
+            <>
+            <Text>{carrera}</Text>
+            <Text>{materia}</Text>
+            <Text>{cuatri}</Text>
+            </>
+             }
+            <Button title=" Ver Perfil" onPress={ ()=>setMostrar(!mostrar)}/>
+
         </View>
     )
 }
