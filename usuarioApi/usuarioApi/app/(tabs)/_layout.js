4d72161d64667a845b4a49c09b708f9767ac0a80
ailingumc <ailingumc@gmail.com>
2026-07-13 23:39:57 -0600

    Practica19 Expo Router

---
+++ usuarioApi/usuarioApi/app/(tabs)/_layout.js
@@ -0,0 +1,34 @@
+import {Tabs} from "expo-router";
+import { Ionicons } from "@expo/vector-icons";
+
+export default function TabsLayout(){
+    return(
+        <Tabs>
+        
+        <Tabs.Screen name="index" options={{title:"Inicio", href:null}} />
+
+        <Tabs.Screen name="alta" options={{ title:"Alta",
+                    tabBarIcon: ({ color, size }) => (
+                        <Ionicons
+                            name="person-add"
+                            color={color}
+                            size={size}
+                        />
+                    )
+                }}
+            />
+
+            <Tabs.Screen name="consulta" options={{ title:"Consulta",
+                    tabBarIcon: ({ color, size }) => (
+                        <Ionicons
+                            name="search"
+                            color={color}
+                            size={size}
+                        />
+                    )
+                }}
+            />
+
+        </Tabs>
+    );
+}
